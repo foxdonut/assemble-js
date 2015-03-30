c80baf2ebb499ebf30ade229bb917b267311e1eb
foxdonut <fdaoud@fastmail.com>
2015-03-30 01:11:02 +0000

    cleanup

--- client/main.js
+++ client/main.js
@@ -11,37 +11,3 @@ module.exports = _.extend(
   require("./books/resource/wire-spec"),
   require("./books/bookList/wire-spec")
 );
-
-//module.exports = {
-  /*
-  $imports: [
-    { module: require("./resource/wire-spec") },
-    { module: require("./books/resource/wire-spec") },
-    { module: require("./books/bookList/wire-spec") }
-  ],
-  */
-
-  /*
-  $imports: [
-    require("./resource/wire-spec"),
-    require("./books/resource/wire-spec"),
-    require("./books/bookList/wire-spec")
-  ],
-  */
-
-  /*
-  $imports: [
-    { module: "./resource/wire-spec" },
-    { module: "./books/resource/wire-spec" },
-    { module: "./books/bookList/wire-spec" }
-  ],
-  */
-
-  /*
-  $imports: [
-    "./resource/wire-spec",
-    "./books/resource/wire-spec",
-    "./books/bookList/wire-spec"
-  ],
-  */  
-//};
