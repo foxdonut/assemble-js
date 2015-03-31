bafd307725c0e97fe5c7431b13c56e1f10ed3b13
foxdonut <fdaoud@fastmail.com>
2015-03-31 02:38:01 +0000

    still trying to figure this out.

--- client/main.js
+++ client/main.js
@@ -1,16 +1,79 @@
 define(function(require, exports, module) {
 
 var _ = require("lodash");
+var aop = require("wire/aop");
 
+/*
 module.exports = _.extend(
   {
     $plugins: [
-      require("wire/aop")/*,
-      require("wire/debug")*/
+      require("wire/aop")*//*,
+      require("wire/debug")*//*
     ]
   },
   require("./books/bookList/wire-spec"), {
+  */
+  /*
+  modelViewBinding: {
+    module: require("knockout"),
+    ready: "applyBindings"
+  }
+  */
+  /*
+  modelViewBinding: {
+    module: require("./modelViewBinding"),
+    ready: "applyBindings"
+  }
+});
+*/
+  
+module.exports = {
+  $plugins: [
+    "wire/aop"
+    /*require("wire/aop")*//*,
+    require("wire/debug")*/
+  ],
+  
+  client: {
+    create: {
+      module: require("./resource/client")
+    }
+  },
 
+  stir: {
+    create: {
+      module: require("./resource/stir"),
+      args: { $ref: "client" }
+    }
+  },
+  
+  bookResource: {
+    create: { $ref: "stir", args: "/books" },
+    ready: "query"
+  },
+
+  bookListViewModel: {
+    module: require("./books/bookList/viewModel"),
+    
+    afterFulfilling: {
+      "bookResource.query": "bookResource.getEntity | books"
+    },
+    
+    afterReturning: {
+      /*
+      deleteBook: "log",
+      log: "bookResource.delete"
+      */
+      deleteBook: "bookResource.delete"
+    }
+  },
+
+  bookListRegistry: {
+    module: require("./component/registry"),
+    init: {
+      register: [ "book-list", require("./books/bookList/component") ]
+    }
+  },
   /*
   modelViewBinding: {
     module: require("knockout"),
@@ -21,6 +84,6 @@ module.exports = _.extend(
     module: require("./modelViewBinding"),
     ready: "applyBindings"
   }
-});
+}
 
 });
