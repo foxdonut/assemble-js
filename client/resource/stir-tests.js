6ad3ad35504d612d79f0e7465793c5d895f40709
foxdonut <fdaoud@fastmail.com>
2015-04-02 23:48:00 +0000

    much simpler test for stir.

--- client/resource/stir-tests.js
+++ client/resource/stir-tests.js
@@ -1,53 +1,16 @@
 require("../test/util/function-bind")();
 
+var test = require("tape");
 var sinon = require("sinon");
+var client = sinon.spy();
+var baseUrl = "/test";
+var stir = require("./stir")(client)(baseUrl);
 
-(function() {  
-  var test = require("tessed");
-  var client = require("./client")();
-  var baseUrl = "/test";
-  var stir = require("./stir")(client)(baseUrl);
+test("issues a GET request for get", function(tt) {
+  tt.plan(1);
 
-  var stirTest = test("stir");
+  var id = 42;
+  stir.get(id);
 
-  stirTest.beforeEach(function(tt, context) {
-    context.server = sinon.fakeServer.create();
-    context.server.autoRespond = true;
-    tt.end();
-  });
-
-  stirTest.afterEach(function(tt, context) {
-    context.server.restore();
-    tt.end();
-  });
-
-  stirTest.test("issues a GET request for get", function(tt, context) {
-    var id = 42;
-
-    context.server.respondWith("GET", baseUrl + "/" + id, [
-      200,
-      { "Content-Type": "application/json" },
-      JSON.stringify({ id: id, title: "Test", author: "Test" })
-    ]);
-
-    stir.get(id).then(function() {
-      tt.end();
-    });
-  });
-})();
-
-(function() {  
-  var test = require("tape");
-  var client = sinon.spy();
-  var baseUrl = "/test";
-  var stir = require("./stir")(client)(baseUrl);
-
-  test("issues a GET request for get", function(tt) {
-    tt.plan(1);
-
-    var id = 42;
-    stir.get(id);
-    
-    tt.ok(client.calledWith({method: "GET", path: baseUrl + "/" + id}), "client called for get");
-  });
-})();
+  tt.ok(client.calledWith({method: "GET", path: baseUrl + "/" + id}), "client called for get");
+});
