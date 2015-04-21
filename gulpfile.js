66f63cc2e50b22d2e47efa40bbe1edb8a68804a4
foxdonut <fdaoud@fastmail.com>
2015-04-21 13:46:11 +0000

    Working on running tape tests through gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var $ = require("gulp-load-plugins")({ lazy: true });
 
 var browserify = require("browserify");
 var vinyl = require("vinyl-source-stream");
+var transform = require("vinyl-transform");
+var shell = require("shelljs");
 
 var config = require("./gulp.config")();
 
@@ -21,10 +23,25 @@ gulp.task("watch", ["browserify"], function() {
   gulp.watch(config.clientSourceFiles, ["browserify"]);
 });
 
+gulp.task("browserify-test", function() {
+  var browserified = transform(function(filename) {
+    return browserify(filename).bundle();
+  });
+
+  return gulp.src(["./client/**/*-test.js"])
+    .pipe(browserified)
+    // .pipe(vinyl("generated-test.js"))
+    .pipe(gulp.dest("./generated-test/"));
+});
+
+gulp.task("test", ["browserify-test"], function() {
+  shell.exec("node_modules/tape/bin/tape test/**/*-test.js | node_modules/faucet/bin/cmd.js");
+});
+
 var serve = function(watch) {
   return $.nodemon(config.serverOptions);
 };
-  
+
 gulp.task("serve", ["browserify"], function() {
   return serve(false);
 });
