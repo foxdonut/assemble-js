6ae857a77d960febb75effe5020c2f5344595b8c
foxdonut <fdaoud@fastmail.com>
2015-04-26 20:12:50 -0400

    added karma-tape-reporter

--- karma.conf.js
+++ karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
+    //reporters: ['tape'],
 
 
     // web server port
