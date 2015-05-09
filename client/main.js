a043a0714a3ac2481ebc114bfeb820031337f5e1
foxdonut <fdaoud@fastmail.com>
2015-05-09 17:44:35 +0000

    I guess this is the way to do it.

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ var Ractive = require("ractive");
 var BookManagerComponent = require("./books/bookManager/component");
 
 new Ractive({
-  el: "body",
+  el: "#app",
   append: true,
   template: "<book-manager></book-manager>",
   components: {
