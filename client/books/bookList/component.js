17b8cb09a35125686c82f20a022de8aa86458502
foxdonut <fdaoud@fastmail.com>
2015-05-12 17:04:33 +0000

    react exercises

--- client/books/bookList/component.js
+++ client/books/bookList/component.js
@@ -1,13 +1,24 @@
-var viewModel = require("./viewModel");
-var template = require("./template.html");
+var React = require("react");
 
-var component = function() {
-  var vm = viewModel();
+var Component = React.createClass({
+  render: function() {
+    var books = this.state.books;
+    return (
+      <div>Book list:</div>
+      <ul>
+      {
+        books.map(function(book) {
+          <li data-field="book" key={book.id}>
+            <button data-action="edit" data-bind="click: $parent.onEdit">Edit</button>
+            <button data-action="delete" data-bind="click: $parent.onDelete">Delete</button>
+             <span data-field="title" data-bind="text: title"></span>
+            (<span data-field="author" data-bind="text: author"></span>)
+          </li>
+        })
+      }
+      </ul>
+    );
+  }
+});
 
-  return {
-    viewModel: vm,
-    template: template
-  };
-};
-
-module.exports = component;
+module.exports = Component;
--- client/main.js
+++ client/main.js
@@ -1,13 +1,65 @@
-var React = require("React");
+var React = require("react");
 var Hello = React.createClass({
   render: function() {
     return (
       <div>Hello, {this.props.name}</div>
     );
   }
-})
+});
+
+var Recipe = React.createClass({
+  getInitialState: function() {
+    return {
+      ingredient: "Hummus"
+    };
+  },
+  render: function() {
+    return (
+      <div>
+      We need {this.state.ingredient}
+      </div>
+    );
+  }
+});
+
+var RecipeList = React.createClass({
+  render: function() {
+    return (
+      <div>
+      RecipeList component text
+      </div>
+    );
+  }
+});
+
+var RecipeForm = React.createClass({
+  render: function() {
+    return (
+      <div>
+      RecipeForm component text
+      </div>
+    );
+  }
+});
+
+var RecipeBook = React.createClass({
+  render: function() {
+    /* component composition == function componsition */
+    return (
+      <div>
+      Hello, world! I am a RecipeBook.
+      <RecipeList/>
+      <RecipeForm/>
+      </div>
+    );
+  }
+});
 
 React.render(
-  <Hello name="React"/>, document.getElementById("app")
+  <div>
+    <div><Hello name="React"/></div>
+    <div><Recipe/></div>
+    <div><RecipeBook/></div>
+  </div>,
+  document.getElementById("app")
 );
-
